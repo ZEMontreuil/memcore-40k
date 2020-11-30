9bd2419f222426a4bc24fac1708673b5fdb7c4a4
ZEMontreuil <zemontreuil@DESKTOP-06JIMO5.localdomain>
2020-11-30 14:44:51 -0600

    Adds data capturing to the game start form.

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ class Player {
     this.score = 0;
   }
 }
+
+class UI {
+  static startMenuEle = document.querySelector('.start-menu');
+  static playerNumEle = UI.startMenuEle.querySelector('#starting-players');
+  static cpNumEle = UI.startMenuEle.querySelector('#starting-cp');
+  static scoreLimitEle = UI.startMenuEle.querySelector('#score-limit');
+  static turnLimitEle = UI.startMenuEle.querySelector('#turn-limit');
+
+  static getStartingInfo (numPlayers, numCP, numScore, numTurn) {
+    // *** TODO *** 
+    // create new player for each number of players
+    // add them to new game 
+  }
+
+  constructor() {
+    UI.startMenuEle.addEventListener("submit", e => {
+      e.preventDefault();
+
+      UI.getStartingInfo(UI.playerNumEle.value, UI.cpNumEle.value,
+        UI.scoreLimitEle.value, UI.turnLimitEle.value);
+    });
+  }
+
+  static game;
+}
+
+let currentUI = new UI();
